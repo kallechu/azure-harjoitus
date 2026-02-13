9df7e794bc62dc917059f2e3a32a3d6aa5674d54
Kalle Kauppila <kallekauppila@kamk.fi>
2026-02-13 11:09:54 +0200

    testing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Tervetuloa Azure CI/CD -harjoitukseen!"
+    return "Tervetuloa Azure CI/CD -harjoitukseen! v2"
 
 if __name__ == "__main__":
     app.run()
